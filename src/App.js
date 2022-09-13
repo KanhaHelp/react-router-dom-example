fcb50f21ea82c4159d1a6d9a27b9c34b6a9849be
KanhaHelp <djkanha044@gmail.com>
2022-09-13 08:17:49 +0530

    all routes added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Signup from "./Signup";
+import About from './About';
+import Contact from './Contact';
+import Login from './Login';
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={ <Home/> } />
         <Route path="/signup" element={ <Signup/> } />
+        <Route path="/login" element={ <Login/> } />
+        <Route path="/about" element={ <About/> } />
+        <Route path="/contact" element={ <Contact/> } />
       </Routes>
   );
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -12,8 +12,8 @@ function Home() {
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
-                    <li style={{float:"right"}}><Link className="active" to="about">Signup</Link></li>
-                    <li style={{float:"right"}}><Link to="about">Login</Link></li>
+                    <li style={{float:"right"}}><Link className="active" to="signup">Signup</Link></li>
+                    <li style={{float:"right"}}><Link to="login">Login</Link></li>
                 </ul>
             </nav>
             <header className="App-header">
