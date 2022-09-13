d0af0da89f3432aa9c02ccbbcbace40160d4b497
KanhaHelp <djkanha044@gmail.com>
2022-09-13 08:22:00 +0530

    updated navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import Signup from "./Signup";
 import About from './About';
 import Contact from './Contact';
 import Login from './Login';
+import './Nav.css';
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={ <Home/> } />
-        <Route path="/signup" element={ <Signup/> } />
-        <Route path="/login" element={ <Login/> } />
-        <Route path="/about" element={ <About/> } />
-        <Route path="/contact" element={ <Contact/> } />
-      </Routes>
+    <>
+      <div className="App">
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+            <li style={{ float: "right" }}><Link className="active" to="signup">Signup</Link></li>
+            <li style={{ float: "right" }}><Link to="login">Login</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,37 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Link } from "react-router-dom";
 import './App.css';
-import './Nav.css';
 
 function Home() {
     return (
-        <div className="App">
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                    <li style={{float:"right"}}><Link className="active" to="signup">Signup</Link></li>
-                    <li style={{float:"right"}}><Link to="login">Login</Link></li>
-                </ul>
-            </nav>
-            <header className="App-header">
 
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </header>
     )
 }
 
